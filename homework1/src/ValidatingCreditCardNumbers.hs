30dbc1bdb8bb4b28b28a04ee579b9426af3aa1d1
ilkecan <ilkecan@protonmail.com>
2021-10-18 16:50:29 +0300

    Implement doubleEveryOther

--- homework1/src/ValidatingCreditCardNumbers.hs
+++ homework1/src/ValidatingCreditCardNumbers.hs
@@ -1,8 +1,33 @@
 module ValidatingCreditCardNumbers where
 
+-- exercise 1
 toDigits :: Integer -> [Integer]
 toDigits = reverse . toDigitsRev
 
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev n | n > 0     = let (q, r) = quotRem n 10 in r : toDigitsRev q
               | otherwise = []
+
+-- exercise 2 - solution 1
+-- doubleEveryOther :: [Integer] -> [Integer]
+-- doubleEveryOther = reverse . doubleEveryOtherRev . reverse
+
+-- doubleEveryOtherRev :: [Integer] -> [Integer]
+-- doubleEveryOtherRev (x0:x1:xs) = x0 : 2 * x1 : doubleEveryOtherRev xs
+-- doubleEveryOtherRev list = list
+
+-- exercise 2 - solution 2
+-- doubleEveryOther :: [Integer] -> [Integer]
+-- doubleEveryOther list = fst $ foldr (\x (acc, bool) -> ((if bool then 2 * x else x) : acc, not bool)) ([], False) list
+
+-- exercise 2 - solution 3
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther list@(x : xs) | even . length $ list = doubleEveryOtherRev list
+                               | otherwise = x : doubleEveryOtherRev xs
+doubleEveryOther list = list
+
+doubleEveryOtherRev :: [Integer] -> [Integer]
+doubleEveryOtherRev = mapCyclic [(* 2), id]
+
+mapCyclic :: [a -> b] -> [a] -> [b]
+mapCyclic = zipWith id . cycle
--- homework1/tests/validating-credit-card-numbers.hs
+++ homework1/tests/validating-credit-card-numbers.hs
@@ -2,7 +2,8 @@ import           Data.Char                      ( digitToInt )
 import           Test.Tasty
 import           Test.Tasty.HUnit
 import           Test.Tasty.QuickCheck         as QC
-import           ValidatingCreditCardNumbers    ( toDigits
+import           ValidatingCreditCardNumbers    ( doubleEveryOther
+                                                , toDigits
                                                 , toDigitsRev
                                                 )
 
@@ -22,6 +23,9 @@ prop_2 :: QC.Positive Integer -> Bool
 prop_2 (QC.Positive number) =
   toDigits number == map (toInteger . digitToInt) (show number)
 
+prop_3 :: [Integer] -> Bool
+prop_3 list = (length . doubleEveryOther) list == length list
+
 qcProps :: TestTree
 qcProps = testGroup
   "QuickCheck properties"
@@ -29,6 +33,7 @@ qcProps = testGroup
   , QC.testProperty
     "toDigits number == map (toInteger . digitToInt) (show number) where number > 0"
     prop_2
+  , QC.testProperty "length . doubleEveryOther == length" prop_3
   ]
 
 unitTests :: TestTree
@@ -38,4 +43,10 @@ unitTests = testGroup
   , testCase "toDigitsRev" $ toDigitsRev 1234 @?= [4, 3, 2, 1]
   , testCase "toDigits 0" $ toDigits 0 @?= []
   , testCase "toDigits negative" $ toDigits (-17) @?= []
+  , testCase "doubleEveryOther empty" $ doubleEveryOther [] @?= []
+  , testCase "doubleEveryOther single" $ doubleEveryOther [3] @?= [3]
+  , testCase "doubleEveryOther even"
+  $   doubleEveryOther [8, 7, 6, 5]
+  @?= [16, 7, 12, 5]
+  , testCase "doubleEveryOther odd" $ doubleEveryOther [1, 2, 3] @?= [1, 4, 3]
   ]
