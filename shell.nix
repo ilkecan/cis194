d9df4887e7ddc87e405770a22389bb0fe582b68f
ilkecan <ilkecan@protonmail.com>
2021-10-27 13:59:36 +0300

    Use the forked QuickCheck until the PR is accepted

--- shell.nix
+++ shell.nix
@@ -1,16 +1,36 @@
-let
-  pkgs = import <nixpkgs> { };
+{ pkgs ? import <nixpkgs> { } }:
 
-  ghcEnv = pkgs.ghc.withPackages (haskellPkgs: with haskellPkgs; [
+let
+  inherit (pkgs)
+    fetchFromGitHub
+    ghc
+    haskell
+    haskellPackages
+    mkShell
+    ;
+
+  # https://github.com/nick8325/quickcheck/pull/336
+  myQC = haskell.lib.overrideSrc haskellPackages.QuickCheck {
+    src = fetchFromGitHub {
+      owner = "ilkecan";
+      repo = "quickcheck";
+      rev = "add-complete-pragma";
+      sha256 = "sha256-AY/lvhc9mtNQdyT1BYy7cWcElIZVKhE2L3PaL83ywv0=";
+    };
+  };
+
+  ghcEnv = ghc.withPackages (haskellPkgs: with haskellPkgs; [
     range
     tasty
     tasty-bench
     tasty-hunit
-    tasty-quickcheck
+    (tasty-quickcheck.override {
+      QuickCheck = myQC;
+    })
   ]);
 in
 
-pkgs.mkShell {
+mkShell {
   packages = with pkgs; with haskellPackages; [
     cabal-install
     ghcEnv
