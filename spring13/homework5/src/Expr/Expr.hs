a2eb5cdc1c7465c8f3fd88f52e65ca71609af907
ilkecan <ilkecan@protonmail.com>
2021-10-30 16:28:28 +0300

    Create instances of Expr

--- spring13/homework5/src/Expr/Expr.hs
+++ spring13/homework5/src/Expr/Expr.hs
@@ -11,3 +11,31 @@ instance Expr ExprT where
   lit n = Lit n
   add expr1 expr2 = Add expr1 expr2
   mul expr1 expr2 = Mul expr1 expr2
+
+instance Expr Integer where
+  lit n = n
+  add expr1 expr2 = expr1 + expr2
+  mul expr1 expr2 = expr1 * expr2
+
+instance Expr Bool where
+  lit n
+    | n > 0 = True
+    | otherwise = False
+  add expr1 expr2 = expr1 || expr2
+  mul expr1 expr2 = expr1 && expr2
+
+newtype MinMax = MinMax Integer
+  deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit n = MinMax n
+  add (MinMax n1) (MinMax n2) = MinMax $ max n1 n2
+  mul (MinMax n1) (MinMax n2) = MinMax $ min n1 n2
+
+newtype Mod7 = Mod7 Integer
+  deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit n = Mod7 $ n `mod` 7
+  add (Mod7 n1) (Mod7 n2) = Mod7 $ (n1 + n2) `mod` 7
+  mul (Mod7 n1) (Mod7 n2) = Mod7 $ (n1 * n2) `mod` 7
--- spring13/homework5/tests/test-Calc.hs
+++ spring13/homework5/tests/test-Calc.hs
@@ -4,10 +4,13 @@ import Calc
   )
 import Expr
   ( Expr (add, lit, mul),
+    MinMax (MinMax),
+    Mod7 (Mod7),
   )
 import ExprT
   ( ExprT (Add, Lit, Mul),
   )
+import Parser (parseExp)
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck as QC
@@ -39,17 +42,31 @@ qcProps =
       QC.testProperty "eval (Mul (Lit n1) (Lit n2)) == n1 * n2" prop_3
     ]
 
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
 unitTests :: TestTree
 unitTests =
   testGroup
     "HUnit tests"
     [ -- Exercise1
       testCase "eval" $ eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) @?= 20,
+      -- Exercise2
       testCase "evalStr valid 1" $ evalStr "2" @?= Just 2,
       testCase "evalStr valid 2" $ evalStr "(2+3)*4" @?= Just 20,
       testCase "evalStr valid 3" $ evalStr "2+3*4" @?= Just 14,
       testCase "evalStr invalid" $ evalStr "2+3*" @?= Nothing,
+      -- Exercise3
       testCase "Expr :: ExprT == ExprT" $
         (mul (add (lit 2) (lit 3)) (lit 4) :: ExprT)
-          @?= Mul (Add (Lit 2) (Lit 3)) (Lit 4)
+          @?= Mul (Add (Lit 2) (Lit 3)) (Lit 4),
+      -- Exercise4
+      testCase "testExp :: Maybe Integer" $
+        (testExp :: Maybe Integer) @?= Just (-7),
+      testCase "testExp :: Maybe Bool" $
+        (testExp :: Maybe Bool) @?= Just True,
+      testCase "testExp :: Maybe MinMax" $
+        (testExp :: Maybe MinMax) @?= Just (MinMax 5),
+      testCase "testExp :: Maybe Mod7" $
+        (testExp :: Maybe Mod7) @?= Just (Mod7 0)
     ]
