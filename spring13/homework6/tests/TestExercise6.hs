116b45cf07e0254e1d12a054f596f85b02954c37
ilkecan <ilkecan@protonmail.com>
2021-11-02 20:57:16 +0300

    Add more tests

--- spring13/homework6/tests/TestExercise6.hs
+++ spring13/homework6/tests/TestExercise6.hs
@@ -9,10 +9,23 @@ import Test.Tasty.HUnit
 test_all :: TestTree
 test_all = testGroup "TestExercise6" [unitTests]
 
+streamSeq1 :: [Int]
+streamSeq1 = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+streamSeq2 :: [Int]
+streamSeq2 = [1, 5, 10, 10, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+streamSeq3 :: [Int]
+streamSeq3 = [-15, -2, -4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
 unitTests :: TestTree
 unitTests =
   testGroup
     "HUnit tests"
     [ testCase "show x" $
-        show x @?= (show . take 20) (0 : 1 : repeat (0 :: Int))
+        show x @?= (show . take 20) (0 : 1 : repeat (0 :: Int)),
+      testCase "x^4" $ show (x ^ (4 :: Int)) @?= show streamSeq1,
+      testCase "(1 + x)^5" $ show ((1 + x) ^ (5 :: Int)) @?= show streamSeq2,
+      testCase "(x^2 + x + 3) * (x - 5)" $
+        show ((x ^ (2 :: Int) + x + 3) * (x - 5)) @?= show streamSeq3
     ]
--- spring13/homework6/tests/TestStream.hs
+++ spring13/homework6/tests/TestStream.hs
@@ -28,19 +28,22 @@ prop4 :: Fun Int Int -> Int -> Bool
 prop4 (Fn f) n =
   (take 20 . streamToList . streamFromSeed f) n == (take 20 . iterate f) n
 
-prop5 :: Integer -> Bool
-prop5 n = (take 1 . streamToList . fromInteger) n == [n]
+prop5 :: Fun Int Int -> Int -> Bool
+prop5 (Fn f) n = streamFromSeed f n == streamFromSeed f n
 
-prop6 :: Fun Integer Integer -> Integer -> Bool
-prop6 (Fn f) n =
-  (negate . streamFromSeed f) n == (streamMap negate . streamFromSeed f) n
+prop6 :: Integer -> Bool
+prop6 n = (take 1 . streamToList . fromInteger) n == [n]
 
 prop7 :: Fun Integer Integer -> Integer -> Bool
 prop7 (Fn f) n =
-  (abs . streamFromSeed f) n == (streamMap abs . streamFromSeed f) n
+  (negate . streamFromSeed f) n == (streamMap negate . streamFromSeed f) n
 
 prop8 :: Fun Integer Integer -> Integer -> Bool
 prop8 (Fn f) n =
+  (abs . streamFromSeed f) n == (streamMap abs . streamFromSeed f) n
+
+prop9 :: Fun Integer Integer -> Integer -> Bool
+prop9 (Fn f) n =
   (signum . streamFromSeed f) n == (streamMap signum . streamFromSeed f) n
 
 qcProps :: TestTree
@@ -53,14 +56,15 @@ qcProps =
         prop2,
       QC.testProperty "streamMap f . streamRepeat == streamRepeat . f" prop3,
       QC.testProperty "streamToList . streamFromSeed f == iterate f" prop4,
-      QC.testProperty "(take 1 . streamToList . fromInteger n) = [n]" prop5,
+      QC.testProperty "streamFromSeed f n = streamFromSeed f n" prop5,
+      QC.testProperty "(take 1 . streamToList . fromInteger n) = [n]" prop6,
       QC.testProperty
         "negate . streamFromSeed f == streamMap negate . streamFromSeed f"
-        prop6,
-      QC.testProperty
-        "abs . streamFromSeed f == streamMap abs . streamFromSeed f"
         prop7,
       QC.testProperty
+        "abs . streamFromSeed f == streamMap abs . streamFromSeed f"
+        prop8,
+      QC.testProperty
         "signum . streamFromSeed f == streamMap signum . streamFromSeed f"
-        prop8
+        prop9
     ]
